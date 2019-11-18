7056750a96495d8028b90cf15348289f91019621
lipwork <56594068+lipwork@users.noreply.github.com>
2019-11-18 10:12:37 +0100

    Add files via upload

--- src/components/SvgFooter.vue
+++ src/components/SvgFooter.vue
@@ -1,35 +1,159 @@
-<template>
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 744.73 373.28" v-bind="$attrs">
-    <rect class="b" x="517.48" y="105.38" width="15.8" height="165.88" />
-    <rect class="b" x="533.15" y="105.38" width="15.8" height="165.88" />
-    <rect class="b" x="548.83" y="105.38" width="15.8" height="165.88" />
-    <rect class="b" x="564.5" y="105.38" width="15.8" height="165.88" />
-    <rect class="b" x="580.17" y="105.38" width="15.8" height="165.88" />
-    <rect class="b" x="580.17" y="105.38" width="15.8" height="165.88" />
-    <rect class="b" x="595.85" y="105.38" width="15.8" height="165.88" />
-    <rect class="b" x="611.52" y="105.38" width="15.8" height="165.88" />
-    <rect class="b" x="627.19" y="105.38" width="15.8" height="165.88" />
-    <rect class="b" x="642.87" y="105.38" width="15.8" height="165.88" />
-    <rect class="b" x="580.33" y="105.38" width="15.8" height="165.88" />
-    <rect class="b" x="569.88" y="121.06" width="15.8" height="165.88" />
-    <rect class="b" x="559.43" y="136.73" width="15.8" height="165.88" />
-    <rect class="b" x="548.98" y="152.4" width="15.8" height="165.88" />
-    <rect class="b" x="538.53" y="168.08" width="15.8" height="165.88" />
-    <rect class="b" x="189.06" y="29.94" width="15.8" height="165.88" />
-    <rect class="b" x="199.51" y="45.61" width="15.8" height="165.88" />
-    <rect class="b" x="209.96" y="61.29" width="15.8" height="165.88" />
-    <rect class="b" x="220.41" y="76.96" width="15.8" height="165.88" />
-    <rect class="b" x="230.85" y="92.63" width="15.8" height="165.88" />
-    <rect class="b" x="131.59" y="61.29" width="15.8" height="165.88" />
-    <rect class="b" x="147.26" y="61.29" width="15.8" height="165.88" />
-    <rect class="b" x="162.94" y="61.29" width="15.8" height="165.88" />
-    <rect class="b" x="178.61" y="61.29" width="15.8" height="165.88" />
-    <rect class="b" x="194.28" y="61.29" width="15.8" height="165.88" />
-    <rect class="b" x="95.02" y="29.94" width="15.8" height="165.88" />
-    <rect class="b" x="105.47" y="45.61" width="15.8" height="165.88" />
-    <rect class="b" x="115.92" y="61.29" width="15.8" height="165.88" />
-    <rect class="b" x="126.37" y="76.96" width="15.8" height="165.88" />
-    <rect class="b" x="136.81" y="92.63" width="15.8" height="165.88" />
+<html>
+<div class="bg"></div>
+<svg version="1.1" id="A" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+   viewBox="0 0 1080 500" style="enable-background:new 0 0 1080 300;" xml:space="preserve" >
+
+<g>
+  <line class="st12" x1="261.1" y1="260.4" x2="344.4" y2="260.4"/>
+  <line class="st12" x1="413.2" y1="293.2" x2="524.3" y2="293.2"/>
+  <line class="st12" x1="242.5" y1="232.6" x2="279.6" y2="288.2"/>
+  <line class="st12" x1="325.9" y1="232.6" x2="362.9" y2="288.2"/>
+  <line class="st12" x1="468.8" y1="293.2" x2="505.8" y2="348.8"/>
+  <line class="st12" x1="733.1" y1="251.5" x2="844.2" y2="251.5"/>
+  <line class="st12" x1="788.6" y1="251.5" x2="825.7" y2="307.1"/>
+  <polyline class="st12" points="574.6,235.1 611.7,290.7 681.1,290.7  "/>
+</g>
+</svg>
+
+
+<svg version="1.1" id="B" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+   viewBox="0 0 1080 500" style="enable-background:new 0 0 1080 300;" xml:space="preserve" >
+
+<g>
+  <line class="st12" x1="274.9" y1="157.7" x2="274.9" y2="304.7"/>
+  <line class="st12" x1="288.8" y1="157.7" x2="288.8" y2="304.7"/>
+  <line class="st12" x1="302.7" y1="157.7" x2="302.7" y2="304.7"/>
+  <line class="st12" x1="316.6" y1="157.7" x2="316.6" y2="304.7"/>
+  <line class="st12" x1="330.5" y1="157.7" x2="330.5" y2="304.7"/>
+</g>
+<g>
+  <line class="st12" x1="242.5" y1="129.9" x2="242.5" y2="276.9"/>
+  <line class="st12" x1="251.8" y1="143.8" x2="251.8" y2="290.8"/>
+  <line class="st12" x1="261.1" y1="157.7" x2="261.1" y2="304.7"/>
+  <line class="st12" x1="270.3" y1="171.6" x2="270.3" y2="318.6"/>
+  <line class="st12" x1="279.6" y1="185.5" x2="279.6" y2="332.5"/>
+</g>
+<g>
+  <line class="st12" x1="325.9" y1="129.9" x2="325.9" y2="276.9"/>
+  <line class="st12" x1="335.1" y1="143.8" x2="335.1" y2="290.8"/>
+  <line class="st12" x1="344.4" y1="157.7" x2="344.4" y2="304.7"/>
+  <line class="st12" x1="353.7" y1="171.6" x2="353.7" y2="318.6"/>
+  <line class="st12" x1="362.9" y1="185.5" x2="362.9" y2="332.5"/>
+</g>
+<g>
+  <line class="st12" x1="413.2" y1="190.5" x2="413.2" y2="337.5"/>
+  <line class="st12" x1="427.1" y1="190.5" x2="427.1" y2="337.5"/>
+  <line class="st12" x1="441" y1="190.5" x2="441" y2="337.5"/>
+  <line class="st12" x1="454.9" y1="190.5" x2="454.9" y2="337.5"/>
+  <line class="st12" x1="468.8" y1="190.5" x2="468.8" y2="337.5"/>
+</g>
+<g>
+  <line class="st12" x1="625.6" y1="188.6" x2="625.6" y2="335.6"/>
+  <line class="st12" x1="639.4" y1="188.6" x2="639.4" y2="335.6"/>
+  <line class="st12" x1="653.3" y1="188.6" x2="653.3" y2="335.6"/>
+  <line class="st12" x1="667.2" y1="188.6" x2="667.2" y2="335.6"/>
+  <line class="st12" x1="681.1" y1="188.6" x2="681.1" y2="335.6"/>
+</g>
+<g>
+  <line class="st12" x1="468.8" y1="190.5" x2="468.8" y2="337.5"/>
+  <line class="st12" x1="482.7" y1="190.5" x2="482.7" y2="337.5"/>
+  <line class="st12" x1="496.5" y1="190.5" x2="496.5" y2="337.5"/>
+  <line class="st12" x1="510.4" y1="190.5" x2="510.4" y2="337.5"/>
+  <line class="st12" x1="524.3" y1="190.5" x2="524.3" y2="337.5"/>
+</g>
+<g>
+  <line class="st12" x1="468.8" y1="190.5" x2="468.8" y2="337.5"/>
+  <line class="st12" x1="478" y1="204.4" x2="478" y2="351.4"/>
+  <line class="st12" x1="487.3" y1="218.3" x2="487.3" y2="365.3"/>
+  <line class="st12" x1="496.5" y1="232.2" x2="496.5" y2="379.2"/>
+  <line class="st12" x1="505.8" y1="246" x2="505.8" y2="393.1"/>
+</g>
+<g>
+  <line class="st12" x1="733.1" y1="148.8" x2="733.1" y2="295.8"/>
+  <line class="st12" x1="747" y1="148.8" x2="747" y2="295.8"/>
+  <line class="st12" x1="760.8" y1="148.8" x2="760.8" y2="295.8"/>
+  <line class="st12" x1="774.7" y1="148.8" x2="774.7" y2="295.8"/>
+  <line class="st12" x1="788.6" y1="148.8" x2="788.6" y2="295.8"/>
+</g>
+<g>
+  <line class="st12" x1="788.6" y1="148.8" x2="788.6" y2="295.8"/>
+  <line class="st12" x1="802.5" y1="148.8" x2="802.5" y2="295.8"/>
+  <line class="st12" x1="816.4" y1="148.8" x2="816.4" y2="295.8"/>
+  <line class="st12" x1="830.3" y1="148.8" x2="830.3" y2="295.8"/>
+  <line class="st12" x1="844.2" y1="148.8" x2="844.2" y2="295.8"/>
+</g>
+<g>
+  <line class="st12" x1="788.6" y1="148.8" x2="788.6" y2="295.8"/>
+  <line class="st12" x1="797.9" y1="162.7" x2="797.9" y2="309.7"/>
+  <line class="st12" x1="807.1" y1="176.6" x2="807.1" y2="323.6"/>
+  <line class="st12" x1="816.4" y1="190.5" x2="816.4" y2="337.5"/>
+  <line class="st12" x1="825.7" y1="204.4" x2="825.7" y2="351.4"/>
+</g>
+<g>
+  <line class="st12" x1="574.6" y1="132.4" x2="574.6" y2="279.4"/>
+  <line class="st12" x1="583.9" y1="146.3" x2="583.9" y2="293.3"/>
+  <line class="st12" x1="593.1" y1="160.1" x2="593.1" y2="307.2"/>
+  <line class="st12" x1="602.4" y1="174" x2="602.4" y2="321"/>
+  <line class="st12" x1="611.7" y1="187.9" x2="611.7" y2="334.9"/>
+</g>
+</svg>
+
+
+
+
+
+<!-- <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 744.73 373.28" v-bind="$attrs" >
+    <rect class="a" x="517.48" y="105.38" width="15.8" height="165.88" />
+    <rect class="a" x="533.15" y="105.38" width="15.8" height="165.88" />
+    <rect class="a" x="548.83" y="105.38" width="15.8" height="165.88" />
+    <rect class="a" x="564.5" y="105.38" width="15.8" height="165.88" />
+    <rect class="a" x="580.17" y="105.38" width="15.8" height="165.88" />
+    <rect class="a" x="580.17" y="105.38" width="15.8" height="165.88" />
+    <rect class="a" x="595.85" y="105.38" width="15.8" height="165.88" />
+    <rect class="a" x="611.52" y="105.38" width="15.8" height="165.88" />
+    <rect class="a" x="627.19" y="105.38" width="15.8" height="165.88" />
+    <rect class="a" x="642.87" y="105.38" width="15.8" height="165.88" />
+    <rect class="a" x="580.33" y="105.38" width="15.8" height="165.88" />
+    <rect class="a" x="569.88" y="121.06" width="15.8" height="165.88" />
+    <rect class="a" x="559.43" y="136.73" width="15.8" height="165.88" />
+    <rect class="a" x="548.98" y="152.4" width="15.8" height="165.88" />
+    <rect class="a" x="538.53" y="168.08" width="15.8" height="165.88" />
+    </svg> -->
+
+  <!-- <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 744.73 373.28" v-bind="$attrs" >
+    <rect class="a" x="517.48" y="105.38" width="15.8" height="165.88" />
+    <rect class="a" x="533.15" y="105.38" width="15.8" height="165.88" />
+    <rect class="a" x="548.83" y="105.38" width="15.8" height="165.88" />
+    <rect class="a" x="564.5" y="105.38" width="15.8" height="165.88" />
+    <rect class="a" x="580.17" y="105.38" width="15.8" height="165.88" />
+    <rect class="a" x="580.17" y="105.38" width="15.8" height="165.88" />
+    <rect class="a" x="595.85" y="105.38" width="15.8" height="165.88" />
+    <rect class="a" x="611.52" y="105.38" width="15.8" height="165.88" />
+    <rect class="a" x="627.19" y="105.38" width="15.8" height="165.88" />
+    <rect class="a" x="642.87" y="105.38" width="15.8" height="165.88" />
+    <rect class="a" x="580.33" y="105.38" width="15.8" height="165.88" />
+    <rect class="a" x="569.88" y="121.06" width="15.8" height="165.88" />
+    <rect class="a" x="559.43" y="136.73" width="15.8" height="165.88" />
+    <rect class="a" x="548.98" y="152.4" width="15.8" height="165.88" />
+    <rect class="a" x="538.53" y="168.08" width="15.8" height="165.88" />
+
+    
+    <rect class="c" x="189.06" y="29.94" width="15.8" height="165.88" />
+    <rect class="c" x="199.51" y="45.61" width="15.8" height="165.88" />
+    <rect class="c" x="209.96" y="61.29" width="15.8" height="165.88" />
+    <rect class="c" x="220.41" y="76.96" width="15.8" height="165.88" />
+    <rect class="c" x="230.85" y="92.63" width="15.8" height="165.88" />
+    <rect class="c" x="131.59" y="61.29" width="15.8" height="165.88" />
+    <rect class="c" x="147.26" y="61.29" width="15.8" height="165.88" />
+    <rect class="c" x="162.94" y="61.29" width="15.8" height="165.88" />
+    <rect class="c" x="178.61" y="61.29" width="15.8" height="165.88" />
+    <rect class="c" x="194.28" y="61.29" width="15.8" height="165.88" />
+    <rect class="c" x="95.02" y="29.94" width="15.8" height="165.88" />
+    <rect class="c" x="105.47" y="45.61" width="15.8" height="165.88" />
+    <rect class="c" x="115.92" y="61.29" width="15.8" height="165.88" />
+    <rect class="c" x="126.37" y="76.96" width="15.8" height="165.88" />
+    <rect class="c" x="136.81" y="92.63" width="15.8" height="165.88" />
+
     <rect class="b" x="439.04" y="169.41" width="15.8" height="165.88" />
     <rect class="b" x="454.71" y="169.41" width="15.8" height="165.88" />
     <rect class="b" x="470.38" y="169.41" width="15.8" height="165.88" />
@@ -40,45 +164,154 @@
     <rect class="b" x="402.46" y="137.34" width="15.8" height="165.88" />
     <rect class="b" x="412.91" y="153.01" width="15.8" height="165.88" />
     <rect class="b" x="423.36" y="168.69" width="15.8" height="165.88" />
-    <rect class="b" x="241.6" y="105.99" width="15.8" height="165.88" />
-    <rect class="b" x="257.28" y="105.99" width="15.8" height="165.88" />
-    <rect class="b" x="272.95" y="105.99" width="15.8" height="165.88" />
-    <rect class="b" x="288.62" y="105.99" width="15.8" height="165.88" />
-    <rect class="b" x="304.3" y="105.99" width="15.8" height="165.88" />
-    <rect class="b" x="304.3" y="105.99" width="15.8" height="165.88" />
-    <rect class="b" x="319.97" y="105.99" width="15.8" height="165.88" />
-    <rect class="b" x="335.64" y="105.99" width="15.8" height="165.88" />
-    <rect class="b" x="351.32" y="105.99" width="15.8" height="165.88" />
-    <rect class="b" x="366.99" y="105.99" width="15.8" height="165.88" />
-    <rect class="b" x="304.3" y="105.99" width="15.8" height="165.88" />
-    <rect class="b" x="314.75" y="121.67" width="15.8" height="165.88" />
-    <rect class="b" x="325.19" y="137.34" width="15.8" height="165.88" />
-    <rect class="b" x="335.64" y="153.01" width="15.8" height="165.88" />
-    <rect class="b" x="346.09" y="168.69" width="15.8" height="165.88" />
-  </svg>
-</template>
+
+    <rect class="d" x="241.6" y="105.99" width="15.8" height="165.88" />
+    <rect class="d" x="257.28" y="105.99" width="15.8" height="165.88" />
+    <rect class="d" x="272.95" y="105.99" width="15.8" height="165.88" />
+    <rect class="d" x="288.62" y="105.99" width="15.8" height="165.88" />
+    <rect class="d" x="304.3" y="105.99" width="15.8" height="165.88" />
+    <rect class="d" x="304.3" y="105.99" width="15.8" height="165.88" />
+    <rect class="d" x="319.97" y="105.99" width="15.8" height="165.88" />
+    <rect class="d" x="335.64" y="105.99" width="15.8" height="165.88" />
+    <rect class="d" x="351.32" y="105.99" width="15.8" height="165.88" />
+    <rect class="d" x="366.99" y="105.99" width="15.8" height="165.88" />
+    <rect class="d" x="304.3" y="105.99" width="15.8" height="165.88" />
+    <rect class="d" x="314.75" y="121.67" width="15.8" height="165.88" />
+    <rect class="d" x="325.19" y="137.34" width="15.8" height="165.88" />
+    <rect class="d" x="335.64" y="153.01" width="15.8" height="165.88" />
+    <rect class="d" x="346.09" y="168.69" width="15.8" height="165.88" />
+
+    
+  </svg> -->
+</html>
 
 <script>
 export default {};
 </script>
 
+
 <style scoped>
-svg {
-  margin-top: -5px;
+.st12{fill:none;stroke:#000000;stroke-miterlimit:10;}
+
+.bg {
+  position: fixed;
+  height: 100%;
+  width: 100%;
+  margin-top: -10px;
   background-size: 1.5rem 1.85rem;
-  background-image: linear-gradient(to right, transparent 3px, transparent 1px),
-    linear-gradient(to bottom, black 5px, transparent 1px);
+  background-image: linear-gradient(to bottom, transparent 1.5px, transparent 1.5px),
+    linear-gradient(to right, black 1.5px, transparent 1.5px);
+
+    animation-name: background_animate;
+    animation-duration: 5s;
+    animation-timing-function: linear;
+    animation-delay: 0s;
+    animation-iteration-count: infinite;
+    animation-direction: alternate;
 }
-.a {
-  fill: none;
+@-webkit-keyframes background_animate {
+  0%{margin-left: 0px;}
+  20% {margin-left: -10px;}
+  40% {margin-left: 10px; }
+  60% {margin-left: -10px; }
+  80% {margin-left: 10px; }
+  100% {margin-left: 0px; }
 }
-.a,
-.b {
+
+@keyframes background_animate {
+  0%{margin-left: 0px;}
+  20% {margin-left: -10px;}
+  40% {margin-left: 10px; }
+  60% {margin-left: -10px; }
+  80% {margin-left: 10px; }
+  100% {margin-left: 0px; }
+}
+
+svg{
+  position: absolute;
+}
+/*.a,
+.b,
+.c,
+.d {
   stroke: #000;
   stroke-miterlimit: 10;
-  stroke-width: 5px;
+  stroke-width: 3px;
+  animation-duration: 5s;
+  animation-timing-function: linear;
+  animation-delay: 0s;
+  animation-iteration-count: infinite;
+  animation-direction: alternate;
 }
+.a {
+  fill: #fff;*/
+
+ /* background-image:linear-gradient(to bottom right,#002f4b,#dc4225);
+  */
+
+/*}
 .b {
-  fill: #fff;
+  fill: red;
+  animation-name: L_animation;
+}
+.c {
+  fill: green;
+  animation-name: H_animation;
+}
+.d {
+  fill: blue;
+  animation-name: T2_animation;
+}*/
+#A{ 
+    animation-name: Tanimation_A;
+    animation-duration: 15s;
+    animation-timing-function: linear;
+    animation-delay: 0s;
+    animation-iteration-count: infinite;
+    animation-direction: alternate;
+}
+@-webkit-keyframes Tanimation_A {
+  0%{top: 0px;}
+  20% {top: -100px;}
+  40% {top: 200px; }
+  60% {top: 100px; }
+  80% {top: -200px; }
+  100% {top: 0px; }
+}
+
+@keyframes Tanimation_A {
+  0%{top: 0px;}
+  20% {top: -100px;}
+  40% {top: 200px; }
+  60% {top: 100px; }
+  80% {top: -200px; }
+  100% {top: 0px; }
+}
+
+#B{
+    animation-name: Tanimation_B;
+    animation-duration: 10s;
+    animation-timing-function: linear;
+    animation-delay: 0s;
+    animation-iteration-count: infinite;
+    animation-direction: alternate;
+}
+@-webkit-keyframes Tanimation_B {
+
+  0%{top: 100px;}
+  20% {top: 300px;}
+  40% {top: 100px; }
+  60% {top: 200px; }
+  80% {top: 0px; }
+  100% {top: 100px; }
+}
+
+@keyframes Tanimation_B {
+  0%{top: 100px;}
+  20% {top: 300px;}
+  40% {top: 100px; }
+  60% {top: 200px; }
+  80% {top: 0px; }
+  100% {top: 100px; }
 }
 </style>
